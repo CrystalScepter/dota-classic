01b0c40516d10bc576f58537b754bee0de3bbbac
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-21 19:11:19 -0300

    Disable Roshan timer block

--- content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
+++ content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
@@ -2,3 +2,5 @@ var hud = $.GetContextPanel().GetParent().GetParent().GetParent();
 var hero_interface = hud.FindChildTraverse("HUDElements").FindChildTraverse("lower_hud").FindChildTraverse("center_with_stats").FindChildTraverse("center_block");
 
 hero_interface.FindChildTraverse("inventory_composition_layer_container").style.visibility = "collapse";
+
+hud.FindChildTraverse("HUDElements").FindChildTraverse("minimap_container").FindChildTraverse("RoshanTimerContainer").style.visibility = "collapse";
