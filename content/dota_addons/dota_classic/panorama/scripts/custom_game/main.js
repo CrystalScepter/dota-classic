80a2235212c373d0f51d2c2af294deb0136f2d7a
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-21 20:06:14 -0300

    Remove innate container from panorama

--- content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
+++ content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
@@ -2,6 +2,7 @@ var hud = $.GetContextPanel().GetParent().GetParent().GetParent();
 var hero_interface = hud.FindChildTraverse("HUDElements").FindChildTraverse("lower_hud").FindChildTraverse("center_with_stats").FindChildTraverse("center_block");
 
 hero_interface.FindChildTraverse("StatBranch").style.visibility = "collapse";
+hero_interface.FindChildTraverse("ContentsContainer").style.visibility = "collapse";
 hero_interface.FindChildTraverse("AghsStatusContainer").style.visibility = "collapse";
 
 hero_interface.FindChildTraverse("inventory_composition_layer_container").style.visibility = "collapse";
