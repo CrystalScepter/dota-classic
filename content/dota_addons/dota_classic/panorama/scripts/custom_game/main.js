71149426490b50fdee0c33b8cb272d6a3beda361
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-21 20:20:52 -0300

    Disable shop buttons at the top

--- content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
+++ content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
@@ -1,5 +1,12 @@
 var hud = $.GetContextPanel().GetParent().GetParent().GetParent();
+var shop_interface = hud.FindChildTraverse("HUDElements").FindChildTraverse("shop").FindChildTraverse("Main").FindChildTraverse("HeightLimiter").FindChildTraverse("GridMainShop");
 var hero_interface = hud.FindChildTraverse("HUDElements").FindChildTraverse("lower_hud").FindChildTraverse("center_with_stats").FindChildTraverse("center_block");
+
+shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("RequestSuggestion").style.visibility = "collapse";
+shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("PopularItems").style.visibility = "collapse";
+shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("ToggleMinimalShop").style.visibility = "collapse";
+shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("ToggleShopFilter").style.visibility = "collapse";
+shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("BuybackProtection").style.visibility = "collapse";
 
 hero_interface.FindChildTraverse("StatBranch").style.visibility = "collapse";
 hero_interface.FindChildTraverse("ContentsContainer").style.visibility = "collapse";
