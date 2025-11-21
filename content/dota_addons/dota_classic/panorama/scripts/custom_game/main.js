82cba3acded13198df80f5beddf1174e3988f1fe
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-21 19:05:07 -0300

    Start work on panorama
    Removed the container on the right side with neutral item upgrade, neutral item slot and TP scroll (TP scroll has been removed from the game as well)

---
+++ content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
@@ -0,0 +1,4 @@
+var hud = $.GetContextPanel().GetParent().GetParent().GetParent();
+var hero_interface = hud.FindChildTraverse("HUDElements").FindChildTraverse("lower_hud").FindChildTraverse("center_with_stats").FindChildTraverse("center_block");
+
+hero_interface.FindChildTraverse("inventory_composition_layer_container").style.visibility = "collapse";
