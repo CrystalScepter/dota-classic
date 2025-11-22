5b38a0b07d17e1f5303582581593394cef85fe46
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-22 13:54:48 -0300

    More item work

---
+++ game/dota_addons/dota_classic/scripts/vscripts/abilities/items/attribute/item_mantle_beta2.lua
@@ -0,0 +1,45 @@
+-- Declare the item class
+item_mantle_beta2 = class({})
+
+-- Link the modifiers that are going to be used by our item
+LinkLuaModifier("modifier_mantle_beta2", "abilities/items/attribute/item_mantle_beta2.lua", LUA_MODIFIER_MOTION_NONE)
+
+-- Return the name of the modifier
+function item_mantle_beta2:GetIntrinsicModifierName()
+	return "modifier_mantle_beta2"
+end
+
+----------------------------------------------------------------------------------------------------
+-- Modifier class
+----------------------------------------------------------------------------------------------------
+
+-- Declare the modifier class
+modifier_mantle_beta2 = class({})
+
+-- Called when the modifier is created
+function modifier_mantle_beta2:OnCreated(kv)
+	-- Retrieve the ability values that are going to be used by our modifier
+	self.bonus_intelligence = self:GetAbility():GetSpecialValueFor("bonus_intelligence")
+end
+
+-- Declare the events and properties that our modifier affects
+function modifier_mantle_beta2:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+	}
+end
+
+-- Give the unit extra intelligence
+function modifier_mantle_beta2:GetModifierBonusStats_Intellect()
+	return self.bonus_intelligence
+end
+
+-- Allow the modifier to stack
+function modifier_mantle_beta2:GetAttributes()
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+-- Prevent the modifier from showing up in the buff bar
+function modifier_mantle_beta2:IsHidden()
+	return true
+end
---
+++ game/dota_addons/dota_classic/scripts/vscripts/abilities/items/attribute/item_slippers_beta2.lua
@@ -0,0 +1,45 @@
+-- Declare the item class
+item_slippers_beta2 = class({})
+
+-- Link the modifiers that are going to be used by our item
+LinkLuaModifier("modifier_slippers_beta2", "abilities/items/attribute/item_slippers_beta2.lua", LUA_MODIFIER_MOTION_NONE)
+
+-- Return the name of the modifier
+function item_slippers_beta2:GetIntrinsicModifierName()
+	return "modifier_slippers_beta2"
+end
+
+----------------------------------------------------------------------------------------------------
+-- Modifier class
+----------------------------------------------------------------------------------------------------
+
+-- Declare the modifier class
+modifier_slippers_beta2 = class({})
+
+-- Called when the modifier is created
+function modifier_slippers_beta2:OnCreated(kv)
+	-- Retrieve the ability values that are going to be used by our modifier
+	self.bonus_agility = self:GetAbility():GetSpecialValueFor("bonus_agility")
+end
+
+-- Declare the events and properties that our modifier affects
+function modifier_slippers_beta2:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+	}
+end
+
+-- Give the unit extra agility
+function modifier_slippers_beta2:GetModifierBonusStats_Agility()
+	return self.bonus_agility
+end
+
+-- Allow the modifier to stack
+function modifier_slippers_beta2:GetAttributes()
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+-- Prevent the modifier from showing up in the buff bar
+function modifier_slippers_beta2:IsHidden()
+	return true
+end
