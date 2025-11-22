69b9b9b1cb2fd3e70f4eaf369d78ffb0e8271ff1
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-22 12:52:45 -0300

    Initial work on items
    Add all 4 variants of Blades of Attack

---
+++ game/dota_addons/dota_classic/scripts/vscripts/abilities/items/offense/item_blades_of_attack_12_beta2.lua
@@ -0,0 +1,45 @@
+-- Declare the item class
+item_blades_of_attack_12_beta2 = class({})
+
+-- Link the modifiers that are going to be used by our item
+LinkLuaModifier("modifier_blades_of_attack_12_beta2", "abilities/items/offense/item_blades_of_attack_12_beta2.lua", LUA_MODIFIER_MOTION_NONE)
+
+-- Return the name of the modifier
+function item_blades_of_attack_12_beta2:GetIntrinsicModifierName()
+	return "modifier_blades_of_attack_12_beta2"
+end
+
+----------------------------------------------------------------------------------------------------
+-- Modifier class
+----------------------------------------------------------------------------------------------------
+
+-- Declare the modifier class
+modifier_blades_of_attack_12_beta2 = class({})
+
+-- Called when the modifier is created
+function modifier_blades_of_attack_12_beta2:OnCreated(kv)
+	-- Retrieve the ability values that are going to be used by our modifier
+	self.bonus_damage = self:GetAbility():GetSpecialValueFor("bonus_damage")
+end
+
+-- Declare the events and properties that our modifier affects
+function modifier_blades_of_attack_12_beta2:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+	}
+end
+
+-- Give the unit extra damage
+function modifier_blades_of_attack_12_beta2:GetModifierPreAttack_BonusDamage()
+	return self.bonus_damage
+end
+
+-- Allow the modifier to stack
+function modifier_blades_of_attack_12_beta2:GetAttributes()
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+-- Prevent the modifier from showing up in the buff bar
+function modifier_blades_of_attack_12_beta2:IsHidden()
+	return true
+end
