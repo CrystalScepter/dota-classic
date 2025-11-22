dae9993bfdcd15437e7cb0b8e7de9758b5445733
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-22 09:21:27 -0300

    Added documentation to panorama

--- content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
+++ content/dota_addons/dota_classic/panorama/scripts/custom_game/main.js
@@ -1,20 +1,26 @@
+// Declare variables
 var hud = $.GetContextPanel().GetParent().GetParent().GetParent();
 var shop_interface = hud.FindChildTraverse("HUDElements").FindChildTraverse("shop").FindChildTraverse("Main").FindChildTraverse("HeightLimiter").FindChildTraverse("GridMainShop");
 var hero_interface = hud.FindChildTraverse("HUDElements").FindChildTraverse("lower_hud").FindChildTraverse("center_with_stats").FindChildTraverse("center_block");
 
+// Disable buttons at the top of the shop
 shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("RequestSuggestion").style.visibility = "collapse";
 shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("PopularItems").style.visibility = "collapse";
 shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("ToggleMinimalShop").style.visibility = "collapse";
 shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("ToggleShopFilter").style.visibility = "collapse";
 shop_interface.FindChildTraverse("SearchAndButtonsContainer").FindChildTraverse("BuybackProtection").style.visibility = "collapse";
 
+// Disable neutral items tab in the shop
 shop_interface.FindChildTraverse("GridHeaderAndMainContent").FindChildTraverse("GridMainContent").FindChildTraverse("GridShopHeaders").FindChildTraverse("GridMainTabs").FindChildTraverse("GridNeutralsTab").style.visibility = "collapse";
 
+// Disable talent tree, innates and Aghanim's indicators
 hero_interface.FindChildTraverse("StatBranch").style.visibility = "collapse";
 hero_interface.FindChildTraverse("ContentsContainer").style.visibility = "collapse";
 hero_interface.FindChildTraverse("AghsStatusContainer").style.visibility = "collapse";
 
+// Disable neutral item and TP scroll slots
 hero_interface.FindChildTraverse("inventory_composition_layer_container").style.visibility = "collapse";
 
+// Disable glyph, radar and Roshan indicators
 hud.FindChildTraverse("HUDElements").FindChildTraverse("minimap_container").FindChildTraverse("GlyphScanContainer").style.visibility = "collapse";
 hud.FindChildTraverse("HUDElements").FindChildTraverse("minimap_container").FindChildTraverse("RoshanTimerContainer").style.visibility = "collapse";
