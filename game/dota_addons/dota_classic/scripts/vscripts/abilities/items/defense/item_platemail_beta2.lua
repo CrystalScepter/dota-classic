f591d9b12422c73ede40170c8b08b5259ff41ef4
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-22 20:40:44 -0300

    More item work

---
+++ game/dota_addons/dota_classic/scripts/vscripts/abilities/items/defense/item_platemail_beta2.lua
@@ -0,0 +1,45 @@
+-- Declare the item class
+item_platemail_beta2 = class({})
+
+-- Link the modifiers that are going to be used by our item
+LinkLuaModifier("modifier_platemail_beta2", "abilities/items/defense/item_platemail_beta2.lua", LUA_MODIFIER_MOTION_NONE)
+
+-- Return the name of the modifier
+function item_platemail_beta2:GetIntrinsicModifierName()
+	return "modifier_platemail_beta2"
+end
+
+----------------------------------------------------------------------------------------------------
+-- Modifier class
+----------------------------------------------------------------------------------------------------
+
+-- Declare the modifier class
+modifier_platemail_beta2 = class({})
+
+-- Called when the modifier is created
+function modifier_platemail_beta2:OnCreated(kv)
+	-- Retrieve the ability values that are going to be used by our modifier
+	self.bonus_armor = self:GetAbility():GetSpecialValueFor("bonus_armor")
+end
+
+-- Declare the events and properties that our modifier affects
+function modifier_platemail_beta2:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+	}
+end
+
+-- Give the unit extra armor
+function modifier_platemail_beta2:GetModifierPhysicalArmorBonus()
+	return self.bonus_armor
+end
+
+-- Allow the modifier to stack
+function modifier_platemail_beta2:GetAttributes()
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+-- Prevent the modifier from showing up in the buff bar
+function modifier_platemail_beta2:IsHidden()
+	return true
+end
