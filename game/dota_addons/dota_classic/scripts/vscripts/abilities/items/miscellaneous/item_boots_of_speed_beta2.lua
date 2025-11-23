315fe1e208879373240f720f237d35a693396e78
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-11-22 21:46:13 -0300

    More item work

--- game/dota_addons/dota_classic/scripts/vscripts/abilities/items/miscellaneous/item_boots_of_speed_beta2.lua
+++ game/dota_addons/dota_classic/scripts/vscripts/abilities/items/miscellaneous/item_boots_of_speed_beta2.lua
@@ -1,12 +1,12 @@
 -- Declare the item class
-item_tranquil_boots_beta2 = class({})
+item_boots_of_speed_beta2 = class({})
 
 -- Link the modifiers that are going to be used by our item
-LinkLuaModifier("modifier_tranquil_boots_beta2", "abilities/items/defense/item_tranquil_boots_beta2.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_boots_of_speed_beta2", "abilities/items/miscellaneous/item_boots_of_speed_beta2.lua", LUA_MODIFIER_MOTION_NONE)
 
 -- Return the name of the modifier
-function item_tranquil_boots_beta2:GetIntrinsicModifierName()
-	return "modifier_tranquil_boots_beta2"
+function item_boots_of_speed_beta2:GetIntrinsicModifierName()
+	return "modifier_boots_of_speed_beta2"
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -14,39 +14,32 @@ end
 ----------------------------------------------------------------------------------------------------
 
 -- Declare the modifier class
-modifier_tranquil_boots_beta2 = class({})
+modifier_boots_of_speed_beta2 = class({})
 
 -- Called when the modifier is created
-function modifier_tranquil_boots_beta2:OnCreated(kv)
+function modifier_boots_of_speed_beta2:OnCreated(kv)
 	-- Retrieve the ability values that are going to be used by our modifier
-	self.bonus_armor = self:GetAbility():GetSpecialValueFor("bonus_armor")
 	self.bonus_movement_speed = self:GetAbility():GetSpecialValueFor("bonus_movement_speed")
 end
 
 -- Declare the events and properties that our modifier affects
-function modifier_tranquil_boots_beta2:DeclareFunctions()
+function modifier_boots_of_speed_beta2:DeclareFunctions()
 	return {
-		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
 		MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
 	}
 end
 
--- Give the unit extra armor
-function modifier_tranquil_boots_beta2:GetModifierPhysicalArmorBonus()
-	return self.bonus_armor
-end
-
 -- Give the unit extra movement speed
-function modifier_tranquil_boots_beta2:GetModifierMoveSpeedBonus_Constant()
+function modifier_boots_of_speed_beta2:GetModifierMoveSpeedBonus_Constant()
 	return self.bonus_movement_speed
 end
 
 -- Allow the modifier to stack
-function modifier_tranquil_boots_beta2:GetAttributes()
+function modifier_boots_of_speed_beta2:GetAttributes()
 	return MODIFIER_ATTRIBUTE_MULTIPLE
 end
 
 -- Prevent the modifier from showing up in the buff bar
-function modifier_tranquil_boots_beta2:IsHidden()
+function modifier_boots_of_speed_beta2:IsHidden()
 	return true
 end
