57004a2978f9b68c7d413f013ba12ca8fce4f5b0
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-12-12 18:32:04 -0300

    Set hull radius for all heroes

--- game/dota_addons/dota_classic/scripts/vscripts/modifiers/modifier_hull_radius.lua
+++ game/dota_addons/dota_classic/scripts/vscripts/modifiers/modifier_hull_radius.lua
@@ -6,10 +6,16 @@ function modifier_hull_radius:OnCreated()
 	-- Proceed if synchronized with the server
 	if IsServer() then
 		-- Proceed if the unit is Shadow Demon and set the custom hull radius
-		if self:GetParent():GetUnitName() == "npc_dota_hero_shadow_demon" then
+		if self:GetParent():GetUnitName() == "npc_dota_hero_elder_titan" then
+			self:GetParent():SetHullRadius(48)
+		-- Proceed if the unit is Shadow Demon and set the custom hull radius
+		elseif self:GetParent():GetUnitName() == "npc_dota_hero_shadow_demon" then
 			self:GetParent():SetHullRadius(0)
-		-- Proceed if the unit is Venomancer and set the custom hull radius
-		elseif self:GetParent():GetUnitName() == "npc_dota_hero_venomancer" then
+		-- Proceed if the unit is Underlord and set the custom hull radius
+		elseif self:GetParent():GetUnitName() == "npc_dota_hero_abyssal_underlord" then
+			self:GetParent():SetHullRadius(0)
+		-- Set a standard hull radius value for heroes if it's none of the listed units
+		else
 			self:GetParent():SetHullRadius(32)
 		end
 	end
