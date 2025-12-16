1a086e7324354a4418219af95f8d882085055fc1
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2025-12-16 20:15:21 -0300

    Change radiant melee creep model

--- game/dota_addons/dota_classic/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/dota_classic/scripts/vscripts/addon_game_mode.lua
@@ -1,6 +1,11 @@
 -- Import game mode class
 require("gamemode")
 
+-- Precache resources
+function Precache(context)
+        PrecacheResource("model", "models/creeps/lane_creeps/creep_2021_radiant/creep_2021_radiant_ranged.vmdl", context)
+end
+
 -- Called when the custom game starts
 function Activate()
         -- Initialize the game mode
