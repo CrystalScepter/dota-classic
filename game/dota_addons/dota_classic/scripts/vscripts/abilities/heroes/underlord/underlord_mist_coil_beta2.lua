6f27f1caf47edbf83651b0690efa9096f01a40e3
Crystal <169555699+CrystalScepter@users.noreply.github.com>
2026-05-27 00:13:11 -0300

    Revert "Mist Coil fix"
    This reverts commit 47c565efc4ad186164fd999a53247eb495213c05.

--- game/dota_addons/dota_classic/scripts/vscripts/abilities/heroes/underlord/underlord_mist_coil_beta2.lua
+++ game/dota_addons/dota_classic/scripts/vscripts/abilities/heroes/underlord/underlord_mist_coil_beta2.lua
@@ -38,8 +38,8 @@ function underlord_mist_coil_beta2:OnProjectileHit(hTarget, vLocation)
 		local damage_table = {
 			victim = hTarget,
 			attacker = caster,
-			damage = damage / 2,
-			damage_type = DAMAGE_TYPE_PURE,
+			damage = damage,
+			damage_type = DAMAGE_TYPE_MAGICAL,
 			ability = self,
 		}
 		ApplyDamage(damage_table)
